049ce3bf013963df4efa389ba64886bfc44a078e
qubadoff <admin@burncode.org>
2025-03-11 01:55:19 +0400

    new

--- resources/views/Frontend/singleOurProjects.blade.php
+++ resources/views/Frontend/singleOurProjects.blade.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Image Details</title>
+    <title>
+        {{ $data->name }}
+    </title>
     <link rel="stylesheet" href="{{ asset('/') }}assets/style.css" />
     <link
         rel="stylesheet"
