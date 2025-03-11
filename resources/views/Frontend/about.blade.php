ac9f99043418badec8ad7dbbac4fbd2f0fe21d09
qubadoff <admin@burncode.org>
2025-03-11 16:21:07 +0400

    new

--- resources/views/Frontend/about.blade.php
+++ resources/views/Frontend/about.blade.php
@@ -25,40 +25,18 @@
             <!-- Contact Info & Form -->
             <h3>Şirkət haqqında</h3><br/>
             <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry’s standard dummy text ever
-                since the 1500s, when an unknown printer took a galley of type and
-                scrambled it to make a type specimen book. It has survived not only
-                five centuries, but also the leap into electronic typesetting,
-                remaining essentially unchanged. It was popularised in the 1960s with
-                the release of Letraset sheets containing Lorem Ipsum passages, and
-                more recently with desktop publishing software like Aldus PageMaker
-                including versions of Lorem Ipsum. <br />
-                <br />
+                ASMOB – 25 illik təcrübə, innovasiya və keyfiyyətin təcəssümü! <br/>
 
-                It is a long established fact that a reader will be distracted by the
-                readable content of a page when looking at its layout. The point of
-                using Lorem Ipsum is that it has a more-or-less normal distribution of
-                letters, as opposed to using ‘Content here, content here’, making it
-                look like readable English. Many desktop publishing packages and web
-                page editors now use Lorem Ipsum as their default model text, and a
-                search for ‘lorem ipsum’ will uncover many web sites still in their
-                infancy. Various versions have evolved over the years, sometimes by
-                accident, sometimes on purpose (injected humour and the like).
-                <br />
-                <br />
-                Contrary to popular belief, Lorem Ipsum is not simply random text. It
-                has roots in a piece of classical Latin literature from 45 BC, making
-                it over 2000 years old. Richard McClintock, a Latin professor at
-                Hampden-Sydney College in Virginia, looked up one of the more obscure
-                Latin words, consectetur, from a Lorem Ipsum passage, and going
-                through the cites of the word in classical literature, discovered the
-                undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-                1.10.33 of “de Finibus Bonorum et Malorum” (The Extremes of Good and
-                Evil) by Cicero, written in 45 BC. This book is a treatise on the
-                theory of ethics, very popular during the Renaissance. The first line
-                of Lorem Ipsum, “Lorem ipsum dolor sit amet..”, comes from a line in
-                section 1.10.32.
+                ASMOB mebel sənayesində yüksək keyfiyyət və yenilikçi yanaşması ilə seçilən brenddir. Biz otellər, restoranlar, iş mərkəzləri və premium mənzillər üçün dəmir, taxta və yumşaq materiallardan möhkəm və estetik mebellər istehsal edirik. <br/>
+
+                Məqsədimiz – müştərilərimizin ehtiyaclarını qarşılamaq və onlara funksional, rahat və uzunömürlü mebellər təqdim etməkdir. Hər bir məhsulumuz dəqiqlik və peşəkarlıqla hazırlanır, müştərilərimizin təqdim etdiyi dizaynlara əsasən istehsal edilir. <br/>
+
+                ✔ İstədiyiniz dizaynda mebel istehsalı – Biz müştərilərimizin təqdim etdiyi dizayn əsasında yüksək keyfiyyətli və dayanıqlı mebellər istehsal edirik. <br/>
+                ✔ 500-dən çox məmnun müştəri – Otellər, restoranlar və biznes sahibləri ASMOB-u keyfiyyət və etibarlılığa görə seçir. <br/>
+                ✔ Tam peşəkar xidmət – İstehsaldan quraşdırmaya qədər hər mərhələdə sizə uyğun həllər təklif edirik. <br/>
+                ✔ 7/24 dəstək – Müştərilərimizin istənilən sualını operativ şəkildə cavablandırmağa hazırıq. <br/>
+
+                ASMOB olaraq, güvən və keyfiyyəti əsas prinsipimiz kimi qoruyur və daim müştərilərimiz üçün ən yaxşısını təqdim edirik. <br/>
             </p>
         </div>
     </div>
